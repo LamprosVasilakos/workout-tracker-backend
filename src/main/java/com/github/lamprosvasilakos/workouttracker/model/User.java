75e8b38d947d441569b38a2f9492c7bcd05899f6
LamprosVasilakos <166054264+LamprosVasilakos@users.noreply.github.com>
2026-01-25 16:30:03 +0200

    update User class to use UUID for id generation

--- src/main/java/com/github/lamprosvasilakos/workouttracker/model/User.java
+++ src/main/java/com/github/lamprosvasilakos/workouttracker/model/User.java
@@ -1,8 +1,8 @@
 package com.github.lamprosvasilakos.workouttracker.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
@@ -11,14 +11,24 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name = "users")
 public class User extends AbstractEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 
     @Override
