e0f6309f1c9e68f577ae203b69b8cc400be58760
LamprosVasilakos <166054264+LamprosVasilakos@users.noreply.github.com>
2026-01-25 04:25:12 +0200

    add User class implementing UserDetails for authentication

---
+++ src/main/java/com/github/lamprosvasilakos/workouttracker/model/User.java
@@ -0,0 +1,28 @@
+package com.github.lamprosvasilakos.workouttracker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User extends AbstractEntity implements UserDetails {
+    private String username;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+}
