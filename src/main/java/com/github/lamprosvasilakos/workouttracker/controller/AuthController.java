1bd1db75684eb0ff6bb2737089da1c0d3924dc6d
LamprosVasilakos <166054264+LamprosVasilakos@users.noreply.github.com>
2026-01-27 03:48:23 +0200

    add register endpoint to AuthController for user registration

--- src/main/java/com/github/lamprosvasilakos/workouttracker/controller/AuthController.java
+++ src/main/java/com/github/lamprosvasilakos/workouttracker/controller/AuthController.java
@@ -1,10 +1,17 @@
 package com.github.lamprosvasilakos.workouttracker.controller;
 
 import com.github.lamprosvasilakos.workouttracker.authentication.AuthenticationService;
-import com.github.lamprosvasilakos.workouttracker.dto.AuthenticationRequest;
-import com.github.lamprosvasilakos.workouttracker.dto.AuthenticationResponse;
+import com.github.lamprosvasilakos.workouttracker.dto.request.AuthenticationRequest;
+import com.github.lamprosvasilakos.workouttracker.dto.request.CreateUserRequest;
+import com.github.lamprosvasilakos.workouttracker.dto.response.AuthenticationResponse;
+import com.github.lamprosvasilakos.workouttracker.dto.response.CreateUserResponse;
+import com.github.lamprosvasilakos.workouttracker.exception.AppObjectAlreadyExistsException;
+import com.github.lamprosvasilakos.workouttracker.exception.ValidationException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +24,21 @@ public class AuthController {
 
     private final AuthenticationService authenticationService;
 
+
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest authenticationRequest) {
-        AuthenticationResponse authenticationResponse = authenticationService.authenticate(authenticationRequest);
-        return ResponseEntity.ok(authenticationResponse);
+        AuthenticationResponse authenticationResponse = authenticationService.login(authenticationRequest);
+        return new ResponseEntity<>(authenticationResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<CreateUserResponse> register(@Valid  @RequestBody CreateUserRequest createUserRequest,BindingResult bindingResult) throws AppObjectAlreadyExistsException, ValidationException {
+
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException(bindingResult);
+        }
+
+        CreateUserResponse createUserResponse = authenticationService.registerUser(createUserRequest);
+        return new ResponseEntity<>(createUserResponse, HttpStatus.CREATED);
     }
 }
