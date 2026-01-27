745d37e0987375bb2616b92fe61db2980ac1e069
LamprosVasilakos <166054264+LamprosVasilakos@users.noreply.github.com>
2026-01-27 17:38:38 +0200

    add Workout entity to represent workouts with properties for date, user, and associated exercises

---
+++ src/main/java/com/github/lamprosvasilakos/workouttracker/entity/Workout.java
@@ -0,0 +1,34 @@
+package com.github.lamprosvasilakos.workouttracker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "workouts")
+public class Workout extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "workout", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<WorkoutExercise> workoutExercises = new ArrayList<>();
+}
