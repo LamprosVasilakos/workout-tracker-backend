a5fc96322c74a4fa165912512e15712d2134ab03
LamprosVasilakos <166054264+LamprosVasilakos@users.noreply.github.com>
2026-01-27 23:58:23 +0200

    add ExerciseResponse DTO for exercise response representation

---
+++ src/main/java/com/github/lamprosvasilakos/workouttracker/dto/response/ExerciseResponse.java
@@ -0,0 +1,13 @@
+package com.github.lamprosvasilakos.workouttracker.dto.response;
+
+import com.github.lamprosvasilakos.workouttracker.entity.MuscleGroup;
+
+import java.util.UUID;
+
+public record ExerciseResponse(
+        UUID id,
+        String name,
+        MuscleGroup muscleGroup,
+        UUID userId
+) {
+}
