d35240386ceb1d31a553f43061678120ef0a4332
LamprosVasilakos <166054264+LamprosVasilakos@users.noreply.github.com>
2026-01-27 17:38:14 +0200

    add MuscleGroup enum to represent different muscle groups

---
+++ src/main/java/com/github/lamprosvasilakos/workouttracker/entity/MuscleGroup.java
@@ -0,0 +1,12 @@
+package com.github.lamprosvasilakos.workouttracker.entity;
+
+public enum MuscleGroup {
+    CHEST,
+    BACK,
+    SHOULDERS,
+    BICEPS,
+    TRICEPS,
+    LEGS,
+    CORE,
+    OTHER
+}
